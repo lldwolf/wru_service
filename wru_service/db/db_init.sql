5717373bc9c0325cbfb5e2f9a2f0e4ee0613ccc4
Lindong <lldwolf@163.com>
2017-11-18 11:00:55 +0800

    init

---
+++ wru_service/db/db_init.sql
@@ -0,0 +1,26 @@
+--用户表
+create table user (
+	id integer primary key autoincrement,
+	phone_num char(11),
+	user_name varchar(50),
+	comments varchar(100));
+	
+insert into user(phone_num, user_name) values('13817024379', '刘林栋');
+insert into user(phone_num, user_name) values('13916253764', '孟祥令');
+	
+--好友表
+create table friend (
+	id integer primary key autoincrement,
+	phone_num1 char(11),
+	phone_num2 char(11)
+);
+
+insert into friend(phone_num1, phone_num2) values('13817024379', '13916253764');
+
+--位置表
+create table location (
+	id integer primary key autoincrement,
+	phone_num char(11),
+	latitude double,
+	longitude double
+);
---
+++ wru_service/src/test/java/wru_service/UserDaoTest.java
@@ -0,0 +1,38 @@
+package wru_service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import lld.wru.dao.UserMapper;
+import lld.wru.model.User;
+import lld.wru.model.UserExample;
+import lld.wru.model.UserExample.Criteria;
+
+public class UserDaoTest {
+    private static final Logger logger = LogManager.getLogger(UserDaoTest.class);
+
+    @Test
+    public void firstTest() throws IOException {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserMapper userDao = (UserMapper) ctx.getBean("userDao");
+        UserExample userExample = new UserExample();
+        Criteria userCriteria = userExample.createCriteria();
+        userCriteria.andPhoneNumEqualTo("13817024379");
+        List<User> users = userDao.selectByExample(userExample);
+        
+        for (User user : users) {
+            Assert.assertNotNull(user);
+            logger.info(user.toString());
+            ((ConfigurableApplicationContext) ctx).close();
+        }
+    }
+
+}
